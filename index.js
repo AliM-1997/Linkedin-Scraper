d8f096d42de053792e67cb124461e1add9e5f6db
AliM-1997 <aliimortada797@gmail.com>
2024-07-06 02:51:00 +0300

    reaching the jobsearch for MLOps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { transposeIterableHandle } = require("puppeteer");
 const puppeteer=require("puppeteer")
-const url="https://www.linkedin.com/jobs/https://www.linkedin.com/jobs/search?keywords=MLOps&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0"
+const url="https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0"
 
 async function run(){
 try {
@@ -8,7 +9,19 @@ try {
     await page.goto(url);
 
     await page.screenshot({path:"screen.png",fullPage: true});
+    
     console.log("printing screen test");
+    await page.waitForSelector('.search-bar__full-placeholder');
+    await page.click(".search-bar__full-placeholder")
+    await page.keyboard.type('MLOps');
+    await page.keyboard.press('Enter');
+
+    await page.waitForSelector('.job-card-list__title');
+
+    await page.click(".base-card")
+    
+    await page.reload();
+    
 
 
 
