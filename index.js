ecd58c16717e2a119b0b050f96e24c7a9258fd57
AliM-1997 <aliimortada797@gmail.com>
2024-07-06 01:57:33 +0300

    adding async function and its block of code for testing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const puppeteer=require("puppeteer")
+const url="https://www.linkedin.com/jobs/https://www.linkedin.com/jobs/search?keywords=MLOps&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0"
+
+async function run(){
+try {
+    const browser=await puppeteer.launch();
+    const page =await browser.newPage();
+    await page.goto(url);
+
+    await page.screenshot({path:"screen.png",fullPage: true});
+    console.log("printing screen test");
+
+
+
+    await browser.close();
+
+} catch (error) {
+    console.error('Error:' + error );
+}
+}
+run();
